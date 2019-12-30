7530923d96fc2af1fe4df738de0ef46b7033744c
15797871606 mapeng0902 <15797871606@163.com>
2019-12-30 15:55:42 +0800

    update Work.js

--- -h5-/back-end/h5-api/api/work/controllers/Work.js
+++ -h5-/back-end/h5-api/api/work/controllers/Work.js
@@ -16,33 +16,17 @@ module.exports = {
 	// strapi-hook-ejs: https://github.com/strapi/strapi/tree/master/packages/strapi-hook-ejs
 	previewOne: async (ctx) => {
 		const work = await strapi.services.work.findOne(ctx.params);
-		http.get("http://pv.sohu.com/cityjson?ie=utf-8",function(datas){
-				var str="";
-				datas.on("datas",function(chunk){
-						str+=chunk;//监听数据响应，拼接数据片段
-				})
-				datas.on("end",function(){
-					localStorage.setItem("ips",str.toString());
-				})
-		})
-		const obj = localStorage.getItem('ips').split('=')[1].split(';')[0];
-		const oo = JSON.parse(obj);
+		var head = ctx.request.header;
+		// console.log(head);
+		const app = head['user-agent'];
 		
-// 		var app1=navigator.appVersion;
-// 		console.log(app1);
-		// console.log(localStorage.getItem("app"));
-		
-		
-		const app = '手机';
-		
-		const ip = oo.cip;
-		const city = oo.cname;
+		const ip = head['x-forwarded-for'];
+		// const city = '';
 		if (work.attributes.status == '1') {
 			const data = await strapi.services.data.create({
 				app,
 				work,
-				ip,
-				city
+				ip
 			});
 		}
 		return ctx.render('engine', {
@@ -50,35 +34,42 @@ module.exports = {
 		});
 
 	},
-	
-  submitForm: async (ctx) => {
-    const work = await strapi.services.work.findOne(ctx.params);
-    const user_id = ctx.request.header.referer.split('&')[1];
-    const formData = ctx.request.body.fields;
-    const workform = await strapi.services.workform.create({ form: formData, work, user_id });
-    ctx.body = { message: 'success', status: 0 };
-  },
-  queryFormsOfOneWork: async (ctx) => {
-    // move to util module or front-end
-    function getUuidMap2Name(work) {
-      const uuidMap2Name = {};
-      work.pages.forEach(page => {
-        page.elements.forEach(ele => {
-          if (ele.name === 'lbp-form-input') {
-            uuidMap2Name[ele.uuid] = ele.pluginProps.placeholder;
-          }
-          if (ele.name === 'lbp-form-radio-group') {
-            uuidMap2Name[ele.uuid] = ele.pluginProps.aliasName;
-          }
-          if (ele.name === 'lbp-form-group') {
-            uuidMap2Name[ele.uuid-1] = ele.pluginProps.userName;
-            uuidMap2Name[ele.uuid +1] = ele.pluginProps.phoneNum;
-            // uuidMap2Name[ele.uuid] = ele.pluginProps.phoneNum
-          }
-        });
-      });
-      return uuidMap2Name;
-    }
+
+	submitForm: async (ctx) => {
+		const work = await strapi.services.work.findOne(ctx.params);
+		const user_id = ctx.request.header.referer.split('&')[1];
+		const formData = ctx.request.body.fields;
+		const workform = await strapi.services.workform.create({
+			form: formData,
+			work,
+			user_id
+		});
+		ctx.body = {
+			message: 'success',
+			status: 0
+		};
+	},
+	queryFormsOfOneWork: async (ctx) => {
+		// move to util module or front-end
+		function getUuidMap2Name(work) {
+			const uuidMap2Name = {};
+			work.pages.forEach(page => {
+				page.elements.forEach(ele => {
+					if (ele.name === 'lbp-form-input') {
+						uuidMap2Name[ele.uuid] = ele.pluginProps.placeholder;
+					}
+					if (ele.name === 'lbp-form-radio-group') {
+						uuidMap2Name[ele.uuid] = ele.pluginProps.aliasName;
+					}
+					if (ele.name === 'lbp-form-group') {
+						uuidMap2Name[ele.uuid - 1] = ele.pluginProps.userName;
+						uuidMap2Name[ele.uuid + 1] = ele.pluginProps.phoneNum;
+						// uuidMap2Name[ele.uuid] = ele.pluginProps.phoneNum
+					}
+				});
+			});
+			return uuidMap2Name;
+		}
 
 		let work = await strapi.services.work.findOne(ctx.params);
 		work = work.toJSON();
